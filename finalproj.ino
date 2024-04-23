febb779e021546ff4693a9abb085130819d29682
sonarpinger <brandonramirez@nevada.unr.edu>
2024-04-23 12:17:22 -0700

    add basic loop and setup w/ previous code also

--- finalproj.ino
+++ finalproj.ino
@@ -4,3 +4,103 @@ Team Members: Brandon Ramirez, Austin Parkerson, Robert Fleming
 Date: 4/23/2024
 */
 
+#include <LiquidCrystal.h>
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2038;
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+/*
+State Descriptions:
+ALL STATES: RTC is used to report time of state transitions, time and changes to stepper motor position are reported
+ALL STATES EXCEPT DISABLED: Humidity & Temp reported every minute to LCD Screen, system responds to changes in vent control, stop button turns fan motor off: turns system to disabled
+DISABLED: yellow led on, to monitoring of temp or water, start button monitored with ISR
+IDLE: green led on, exact timestamp records transition times, water level is mononitored: if too low, error state: red led
+ERROR: motor does not start automatically, reset button triggers change to IDLE state iff water level is above threshold, error message displayed on LCD, red led
+RUNNING: fan motor on, transistion to idle on temp drop below thresh, transition to error if water level becomes to low, blue led is on
+*/
+
+enum State {DISABLED, IDLE, ERROR, RUNNING};
+
+void setup(){
+  // setup the UART
+  U0init(9600);
+  // setup the ADC
+  adc_init();
+}
+
+void loop(){
+  // // check if the start button is pressed
+  // if(*myPIND & 0x04){
+  //   // check if the system is in the disabled state
+  //   if(state == DISABLED){
+  //     // change the state to idle
+  //     state = IDLE;
+  //     // turn off the yellow LED
+  //     *myPORTB &= 0b11111011;
+  //     // turn on the green LED
+  //     *myPORTB |= 0b00000100;
+  //     // report the state transition
+  //     U0putchar('D');
+  //     U0putchar('I');
+  //     U0putchar(' ');
+  //     U0putchar('T');
+}
+
+void adc_init(){
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num){
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
+void U0init(int U0baud){
+ unsigned long FCPU = 16000000;
+ unsigned int tbaud;
+ tbaud = (FCPU / 16 / U0baud - 1);
+ // Same as (FCPU / (16 * U0baud)) - 1;
+ *myUCSR0A = 0x20;
+ *myUCSR0B = 0x18;
+ *myUCSR0C = 0x06;
+ *myUBRR0  = tbaud;
+}
+unsigned char U0kbhit(){
+  return *myUCSR0A & RDA;
+}
+unsigned char U0getchar(){
+  return *myUDR0;
+}
+void U0putchar(unsigned char U0pdata){
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
